b9892ff2c77cfada920e65be7a56a452b4678aa1
Nihar Zutshi <59330701+Nihar-Zutshi1@users.noreply.github.com>
2021-05-30 20:13:37 +0530

    Code

---
+++ Studentbank.java
@@ -0,0 +1,15 @@
+public class Studentbank {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        Account tim = new Account("Nihar Zutshi", "A00001");
+
+
+        Account Nihar = new Account("Nihar Zutshi", "Jk--02");
+        Nihar.showMenu();
+
+
+    }
+
+}
